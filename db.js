db6ce06ed5df37d5740b59c5bf61733cceab6e4a
Fran Marasco <francisco.marasco@gmail.com>
2024-09-09 09:51:03 -0300

    update to front

--- db.js
+++ db.js
@@ -1,14 +1,21 @@
-// db.js
 import mongoose from 'mongoose';
 
-const uri = 'your_mongodb_connection_string'; // Replace with your MongoDB connection string
+const uri = 'mongodb://127.0.0.1:27017/talently'; // Replace with your MongoDB connection string
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 const personSchema = new mongoose.Schema({
   name: { type: String, unique: true },
+  age: Number,
+  email: String,
   // Add other fields as per your data structure
-});
+  // For example:
+  // role: String,
+  // salary: Number,
+  // etc.
+}, { strict: false }); // Use strict: false to allow saving fields not defined in the schema
 
 const Person = mongoose.model('Person', personSchema);
 
--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 import fetch from 'node-fetch';
 import Person from './db.js'; // Import the Person model
-
+const firstPage = parseInt(process.argv[2], 10) || 1;
 const baseUrl = 'https://hub-admin.talently.tech/api/v4/hire/candidates';
 const params = {
-  role_id: 2,
-  stacks: [51],
-  vetting: 1,
-  english_proficiency_ids: [3],
+  role_id: 4, 
+  vetting: 1, 
   min_salary: 2000,
-  max_salary: 5000,
+  max_salary: 7000,
   initial: 0,
+  country: 'Argentina',
   work_offer_id: 23321,
-  page: 1 // Start with page 1
+  page: 1
 };
 
 const options = {
@@ -38,11 +37,24 @@ const options = {
 
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
+const fetchCandidateDetails = async (candidateId) => {
+  const candidateUrl = `https://hub-admin.talently.tech/api/v4/hire/candidate/${candidateId}?work_offer_id=${params.work_offer_id}&role_id=${params.role_id}`;
+  try {
+    const response = await fetch(candidateUrl, options);
+    const data = await response.json();
+    return data.result.match_user;
+  } catch (error) {
+    console.error(`Error fetching details for candidate ${candidateId}:`, error);
+    return null;
+  }
+};
+
 const fetchPage = async (page) => {
   params.page = page;
-  const url = `${baseUrl}?${new URLSearchParams(params).toString()}`;
-
+  const url = `${baseUrl}?stacks\[\]=14&stacks\[\]=708&stacks\[\]=14&stacks\[\]=86&vetting=1&english_proficiency_ids\[\]=3&${new URLSearchParams(params).toString()}`;
+  console.log(url);
   try {
+    console.log('fetching...')
     const response = await fetch(url, options);
     const data = await response.json();
 
@@ -53,12 +65,22 @@ const fetchPage = async (page) => {
 
     for (const item of data.result.data) {
       try {
+        // Check if the name already exists in the database
+        const existingPerson = await Person.findOne({ name: item.name });
+        if (existingPerson) {
+          console.log(`Skipping: ${item.name} already exists in the database`);
+          continue;
+        }
+
+        await delay(getRandomDelay(1, 2)); 
+        const extraData = await fetchCandidateDetails(item.id);
         await Person.updateOne(
           { name: item.name }, // Assuming 'name' is the unique field
-          { $set: item },
+          { $set: { ...item, extra_data: extraData } },
           { upsert: true } // Create a new document if no match is found
         );
-        console.log(`Saved/Updated: ${item.name}`);
+        console.log(`Saved/Updated: ${item.name} from ${item.country}`);
+
       } catch (error) {
         console.error(`Error saving ${item.name}:`, error);
       }
@@ -70,17 +92,21 @@ const fetchPage = async (page) => {
     return false;
   }
 };
-
+const getRandomDelay = (minx, maxx) => {
+  const min = minx * 1000; // 5 seconds
+  const max = maxx * 1000; // 15 seconds
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 const fetchAllPages = async () => {
-  let page = 1;
   let hasMore = true;
+  let page = firstPage;
 
   while (hasMore) {
     console.log(`Fetching page ${page}`);
     hasMore = await fetchPage(page);
     if (!hasMore) break;
     page++;
-    await delay(10000); // Wait for 10 seconds before fetching the next page
+    await delay(getRandomDelay(1, 3)); // Wait for 10 seconds before fetching the next page
   }
 };
 
